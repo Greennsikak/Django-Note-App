dd30099524fb8e3f1b12a39a6d3aaf12ee7a004c
Nsikak Etim <nsikaketim744@gmail.com>
2020-06-25 13:55:09 +0100

    Ready for deployment to heroku

--- note/urls.py
+++ note/urls.py
@@ -6,11 +6,11 @@ app_name = "Note"
 
 urlpatterns = [
     path('', views.topic, name='topic'),
-    path('topic/<int:topic_id>/', views.topics, name='topics'),
-    path('topic/new_topic/', views.new_topic, name='new_topic'),
-    path('topic/<int:topic_id>/delete', views.del_topic, name='del_topic'),
-    path('topic/<int:topic_id>/new_entry/', views.new_entry, name='new_entry'),
-    path('topic/<int:entry_id>/edit_entry/', views.edit_entry, name='edit_entry')
+    path('note/<int:topic_id>/', views.topics, name='topics'),
+    path('note/new_topic/', views.new_topic, name='new_topic'),
+    path('note/<int:topic_id>/delete', views.del_topic, name='del_topic'),
+    path('note/<int:topic_id>/new_entry/', views.new_entry, name='new_entry'),
+    path('note/<int:entry_id>/edit_entry/', views.edit_entry, name='edit_entry')
 
 ]
 
